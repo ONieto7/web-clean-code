3eab511fe7212e226f715c385ee52acce0ba94d0
ONieto7 <oscarniro@gmail.com>
2025-06-27 18:21:50 +0200

    add tests for the Button component and navigation in UserList

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,1 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../components/Button';
 
+test('renders button with text', () => {
+  render(<Button>Click me</Button>);
+  expect(screen.getByText(/Click me/i)).toBeInTheDocument();
+});
--- src/test/UserList.test.js
+++ src/test/UserList.test.js
@@ -1,1 +1,27 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import UserList from '../views/UserList';
 
+jest.mock('../repositories/userRepository', () => ({
+  getAll: jest.fn(() =>
+    Promise.resolve({
+      data: [
+        { id: 1, first_name: 'Emma', last_name: 'Wong', email: 'emma.wong@reqres.in', avatar: 'avatar1.jpg' },
+        { id: 2, first_name: 'Charles', last_name: 'Morris', email: 'charles.morris@reqres.in', avatar: 'avatar2.jpg' }
+      ],
+      total_pages: 1,
+      page: 1
+    })
+  )
+}));
+
+test('navigates to user detail on user card click', async () => {
+  render(
+    <MemoryRouter>
+      <UserList />
+    </MemoryRouter>
+  );
+
+  const userCard = await screen.findByText(/Emma Wong/i);
+  fireEvent.click(userCard);
+});
