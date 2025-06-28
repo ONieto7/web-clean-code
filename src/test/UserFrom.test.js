db3e1d6cd8c4f29c9faf98df9f329541d0fae468
ONieto7 <oscarniro@gmail.com>
2025-06-28 12:08:29 +0200

    Refactor tests to use MemoryRouter and downgrade react-router-dom
    - Downgraded react-router-dom from version 7.6.2 to 6.30.1 in package.json.
    - Added MemoryRouter tests in UserDetail.test.js, UserFrom.test.js, and UserList.test.js.
    - Removed existing UserList component tests and replaced them with a simple MemoryRouter test.

--- src/test/UserFrom.test.js
+++ src/test/UserFrom.test.js
@@ -1,1 +1,13 @@
 
+// src/__tests__/MemoryRouter.test.jsx
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+test('MemoryRouter import works', () => {
+  render(
+    <MemoryRouter>
+      <div>Test</div>
+    </MemoryRouter>
+  );
+});
